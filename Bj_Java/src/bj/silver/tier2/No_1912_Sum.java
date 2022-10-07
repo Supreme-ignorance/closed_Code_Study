1a2f97d697984962d2a12aed765ae932876a2f70
mg-glter <rjsdlzoq@naver.com>
2022-10-07 14:47:20 +0900

    이동

--- Bj_Java/src/bj/silver/tier2/No_1912_Sum.java
+++ Bj_Java/src/bj/silver/tier2/No_1912_Sum.java
@@ -3,6 +3,8 @@ package bj.silver.tier2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.StringTokenizer;
 
 public class No_1912_Sum {
@@ -24,5 +26,34 @@ public class No_1912_Sum {
 		}
 		
 		System.out.println(max);
+	}
+}
+class Exercise implements Comparable<Exercise>{
+	Date recent;
+	int value;
+	
+	public Exercise(int value) {
+		super();
+		this.recent = new Date();
+		this.value = value;
+	}
+
+	@Override
+	public int compareTo(Exercise o) {
+		Date now = new Date();
+		Date date = this.recent;
+
+		Calendar cal = Calendar.getInstance(); 
+		cal.setTime(date);
+		cal.add(Calendar.DATE, 2); //2일 더하기
+		int dateDay = cal.DATE;
+		
+		cal.setTime(now);
+		int nowDay = cal.DATE;
+		
+		if (dateDay < nowDay) {
+			return this.value - o.value;
+		} else 
+			return 0 - o.value;
 	}
 }
